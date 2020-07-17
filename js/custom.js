e4cb8a2f00b41c0b58d9e517e3c98fe0ae92499d
Nikhil-Thummar <68430050+Nikhil-Thummar@users.noreply.github.com>
2020-07-17 19:05:40 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+//Project Slider
+$('#project').owlCarousel( {
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: true,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1200: {
+            items: 3
+        }
+    }
+});
+
+//Team Slider
+$('#team').owlCarousel( {
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: true,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1200: {
+            items: 3
+        }
+    }
+});
+
+//Happy Client Slider
+$('#hclient').owlCarousel( {
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: true,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1200: {
+            items: 1
+        }
+    }
+});
